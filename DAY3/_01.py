cd2dd3c9fe573fffeab089fdfee53166c44ce467
rishithayadav2231-art <rishithayadav2231@gmail.com>
2026-08-25 11:29:13 +0530

    4g4g

--- DAY3/_01.py
+++ DAY3/_01.py
@@ -1,5 +1,5 @@
-n=int(input("enter your factorial number"))
-fact=1
-for i in range(1,n+1):
-    fact=fact*i
+n=int(input("enter your fatorial number"))
+fact =1
+for i in range (1,n+1):
+    fact =fact*i
 print("the factorial number is",fact)
